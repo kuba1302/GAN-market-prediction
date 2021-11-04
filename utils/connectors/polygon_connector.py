6db8c5130adc4708557e43e0df0e6279e9e8ca8a
kuba1302 <jakub.wujec@perfect-data.pl>
2021-11-04 21:01:46 +0100

    added polygon and working on sql

--- utils/connectors/polygon_connector.py
+++ utils/connectors/polygon_connector.py
@@ -1,26 +1,29 @@
 import datetime
-import typing_extensions
 import urllib.request
 import json
 
 API_KEY = 'uAbu9bSmDF6XkqOuBawYuvdBspoueBys'
 
 
-def get_one_day_data(stocksTicker, date, api_key, adjusted=True):
-    if adjusted:
-        adjusted_str = 'true'
-    else: 
-        adjusted_str = 'false'
+class PolygonConnector: 
 
-    url = f'https://api.polygon.io/v1/open-close/AAPL/{date}?' \
-    f'adjusted={adjusted_str}&apiKey={api_key}'
-    print(url)
-    response = urllib.request.urlopen(url)
+    def __init__(self, api_key) -> None:
+        self.api_key = api_key
 
-    return json.loads(response.read())
+    def get_one_day_data(self, ticker, date, adjusted=True):
+        if adjusted:
+            adjusted_str = 'true'
+
+        else: 
+            adjusted_str = 'false'
+
+        url = f'https://api.polygon.io/v1/open-close/{ticker}/{date}?' \
+        f'adjusted={adjusted_str}&apiKey={self.api_key}'
+        response = urllib.request.urlopen(url)
+
+        return json.loads(response.read())
 
 
-data = get_one_day_data('ATVI', '2021-11-02', api_key=API_KEY)
-print(data)
+
 
 
--- utils/connectors/sql_connector.py
+++ utils/connectors/sql_connector.py
@@ -1,10 +1,23 @@
 import psycopg2
 import os 
 
-def connect_to_cloud_sql(db_name):
-    return psycopg2.connect(
-        host = os.environ['PSG_HOST'], 
-        user = os.environ['PSG_USER'], 
-        password = os.environ['PSG_PSWD'], 
-        database = db_name
-    )
+"""
+TO DO: 
+Add INSERT JSON TO TABLE DATA 
+USE COMPANY SHORT NAME MAPPING FROM JSON
+"""
+class SqlConnector: 
+
+    def __init__(self, db_name) -> None:
+        self.connection = self.connect_to_cloud_sql(db_name)
+        self.cursor = self.connection.cursor
+        
+    def connect_to_cloud_sql(self, db_name):
+        return psycopg2.connect(
+            host = os.environ['PSG_HOST'], 
+            user = os.environ['PSG_USER'], 
+            password = os.environ['PSG_PSWD'], 
+            database = db_name
+        )
+
+    
