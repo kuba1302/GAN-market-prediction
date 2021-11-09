16d9c54fe9b116e9aa551cb445e2f4ec2b88297e
kuba1302 <jakub.wujec@perfect-data.pl>
2021-11-09 09:18:52 +0100

    prepared bert_base

---
+++ utils/bert/model_base.py
@@ -0,0 +1,31 @@
+from transformers import (AutoTokenizer, 
+                          AutoModelForSequenceClassification, 
+                          )
+
+import tensorflow as tf
+
+
+class BertBase: 
+
+    def __init__(self):
+
+        self.tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+        self.model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
+
+    def tokenize(self, sentence): 
+        return self.tokenizer(sentence, return_tensors='pt')
+
+    def predict(self, tokenized_sentence): 
+        model_return = self.model(**tokenized_sentence)
+        scores = model_return[0][0].detach().numpy()
+        scores = tf.nn.softmax(scores)
+        # (negative, positive)
+        return (scores[0].numpy(), scores[1].numpy())
+
+    def tokenize_and_predict(self, sentence): 
+        return self.predict(self.tokenize(sentence))
+
+
+if __name__ == '__main__': 
+    bert = BertBase() 
+    print(bert.tokenize_and_predict('Apple is a bad company'))
