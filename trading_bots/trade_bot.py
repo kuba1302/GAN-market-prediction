e940794bea3d879a910f4e9895bc424f06ec6d0d
kuba1302 <kuba1302@gmail.com>
2022-04-03 00:28:39 +0200

    add StockTimeGan class saving

--- trading_bots/trade_bot.py
+++ trading_bots/trade_bot.py
@@ -1,14 +1,25 @@
 import pandas as pd 
 import numpy as np 
-
+import dill 
 
 
 class TradeBot: 
 
-    def __init__(self, transaction_cost: int, currency_count: int, asset_count: int = 0):
+    def __init__(self, transaction_cost: int, currency_count: int, ticker: str, asset_count: int = 0):
         self.currency_count = currency_count
         self.asset_count = asset_count
         # at the beggining balance is our currency amount 
         self.current_balance = currency_count
-        self.current_price = None 
         self.transaction_cost = transaction_cost
+        self.ticker = ticker
+        self.current_price = None 
+        self.model = None 
+
+    def __repr__(self):
+        return f'Trade Bot - Ticker: {self.ticker} - Balance: {self.current_balance}'
+
+    def load_model(self, path): 
+        with open(path, 'r') as handle: 
+            self.model = dill.load(handle)
+    
+    
